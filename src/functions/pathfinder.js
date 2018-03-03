f334be4fded1e7503a65ffa19d7a4aa9c9a6886e
David Xu <davidxu1991@gmail.com>
2018-03-03 16:32:39 +1300

    implemented the breath first search

--- src/functions/pathfinder.js
+++ src/functions/pathfinder.js
@@ -5,8 +5,8 @@ const calcPath = (array, width, start, end) => {
         start !== undefined &&
         end !== undefined
     ) {
-        return depthFirstCalc(array, width, start, end)
-        //return breathFirstCalc(array, width, start, end)
+        //return depthFirstCalc(array, width, start, end)
+        return breathFirstCalc(array, width, start, end)
     } else {
         return []
     }
@@ -15,7 +15,6 @@ const calcPath = (array, width, start, end) => {
 const findNeighbors = (array, width, node) => {
     let result = []
 
-    //TODO: Handle when the nodes are against the edges
     if (node % width < width - 1 && !array[node + 1].blocked) {
         result.push(node + 1)
     }
@@ -32,6 +31,45 @@ const findNeighbors = (array, width, node) => {
         result.push(node - width)
     }
     return result
+}
+
+const breathFirstCalc = (array, width, start, end) => {
+    let queue = [];
+    let parents = [];
+    let found = false;
+
+    queue.push(start);
+    parents[start] = -1;
+
+    while (queue.length > 0 && !found) {
+        const node = queue.shift();
+        const neighbors = findNeighbors(array, width, node);
+        neighbors.forEach(neighbor => {
+            if (parents[neighbor] === undefined) {
+                parents[neighbor] = node;
+                if (neighbor === end) {
+                    found = true;
+                } else {
+                    queue.push(neighbor);
+                }
+            }
+        })
+    }
+
+    let path = findPath(parents, end);
+
+    return path;
+
+}
+
+const findPath = (parentsArray, end) => {
+    let path = [];
+    let node = end;
+    while (parentsArray[node] !== -1 && parentsArray[node] !== undefined) {
+        path.push(node);
+        node = parentsArray[node];
+    }
+    return path;
 }
 
 const depthFirstCalc = (array, width, start, end) => {
@@ -81,6 +119,5 @@ const depthFirstCalc = (array, width, start, end) => {
     return path
 }
 
-//const breathFirstCalc = (array, width, start, end) => {}
 
 export default calcPath
